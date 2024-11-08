f0d521db897cfa1a5def0ac222e191ef6af52c8e
AdityaNPatil <adityapatil2708@gmail.com>
2024-11-08 14:41:53 +0530

    Footer created

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <footer className="py-8 bg-blue-800 text-center text-white">
+      <p className="text-sm">© {new Date().getFullYear()} BlinkLinks. All rights reserved.</p>
+      <div className="flex justify-center gap-6 mt-4">
+        <Link href="https://linkedin.com" target="_blank">
+          <Image src="/linkedin-icon.png" alt="LinkedIn" height={24} width={24} />
+        </Link>
+        <Link href="https://github.com/AdityaNPatil" target="_blank">
+          <Image src="/github-icon.png" alt="GitHub" height={24} width={24} />
+        </Link>
+      </div>
+    </footer>
+  );
+}
